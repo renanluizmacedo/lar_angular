d8bb34d1a2e6f30e86c5af8c392b660c61e5ed98
renanluizmacedo <renanluizmacedo@gmail.com>
2021-07-26 17:34:21 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Laravel</title>
+</head>
+<body>
+    
+
+    <app-root></app-root>
+    <script src="{{asset('/js/runtime.bc4ce951e505b9e7d62d.js')}}" defer></script>
+    <script src="{{asset('/js/polyfills.f161bf4a91cb3d66b739.js')}}" defer></script>
+    <script src="{{asset('/js/main.75ffc0fee54cc2847e5f.js')}}" defer></script>
+</body>
+</html>
